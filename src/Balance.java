23d353002c48a80a343a4ab95c7d7716cb82d359
rnail1989 <rnail1989@mail.ru>
2020-10-15 21:49:39 +0300

    Update Formula

--- src/Balance.java
+++ src/Balance.java
@@ -1,21 +1,29 @@
 public class Balance {
 
+    private  double balance;
 
-    public Balance(double balance) {
-        double currentBalance;
-        public double setCurrentBalance(double currentBalance) {
-            currentBalance = currentBalance + this.currentBalance;
-            return currentBalance;
+    public Balance(double Balance) {
+        this.balance = balance;
 
-        }
-
-        public double getCurrentBalance() {
-            currentBalance = currentBalance - this.currentBalance;
-            return currentBalance;
-        }
     }
 
+    public Balance() {
 
+    }
+        public double getBalance() {
+            return balance;
+
+        }
+        public double setBalance(double balance) {
+        this.balance=balance;
+
+            return balance;
+        }
+
+
+    public double setBalance() {
+        return balance;
+    }
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         Balance balance = new Balance(100.0);
-        balance.getCurrentBalance(500);
-        balance.setCurrentBalance(300);
+        double balanceInfo = balance.getBalance();
+        double balanceNew = balance.setBalance(-500);
+
+
+
         System.out.println(balance);
-        System.out.println(balance.getCurrentBalance());
-        System.out.println(balance.setCurrentBalance());
+        System.out.println(balance.getBalance());
+        System.out.println(balance.setBalance());
         System.out.println(balance);
 
 
